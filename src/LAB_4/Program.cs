e76c9cd973609894df78a28b02a909c534a08823
Dmytro Kryvenko <yungmirage23@gmail.com>
2025-02-19 11:22:09 +0200

    chore: added LAB_4

---
+++ src/LAB_4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LAB_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static void Task1()
+        {
+
+            ///wqdqwdwqkdopqwkd
+        }
+    }
+}
